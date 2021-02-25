6ec96e5a05d993227a8e1f2ece2e3b4048d6e2b1
antraahex <antra@ahex.co.in>
2021-02-25 14:20:00 +0530

    initialChanges

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { ColorPickerComponent } from './color-picker/color-picker.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'colorPicker', component: ColorPickerComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
